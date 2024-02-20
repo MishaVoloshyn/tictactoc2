3da73de775c79d71c6946d3ba5933ddbe90562b1
MISHA <vol.mykhailo@gmail.com>
2024-02-20 21:25:33 +0200

    Добавьте файлы проекта.

---
+++ hw-13-02-cross/resource.h
@@ -0,0 +1,38 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется hw-13-02-cross.rc
+//
+#define IDC_MYICON                      2
+#define IDD_HW1302CROSS_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_HW1302CROSS                 107
+#define IDI_SMALL                       108
+#define IDC_HW1302CROSS                 109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
+#define IDC_BUTTON7                     1006
+#define IDC_BUTTON8                     1007
+#define IDC_BUTTON9                     1008
+#define IDC_CHECK1                      1040
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ hw-13-02-cross/tictactoc.cpp
@@ -0,0 +1,357 @@
+﻿// hw-13-02-cross.cpp : Определяет точку входа для приложения.
+//
+
+#include <windows.h>
+#include <tchar.h>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <time.h>
+#include "Resource.h"
+
+#define BUTTON_COUNT 9
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+using namespace std;
+
+HWND hCheck;
+vector<HWND>Buttons;
+vector<int>chosen;
+
+void Check(HWND hDlg, WPARAM wParam, LPARAM lParam);
+void InitButton(HWND hDlg, WPARAM wParam, LPARAM lParam);
+
+
+void Check(HWND hDlg, WPARAM wParam, LPARAM lParam)
+{
+    hCheck = GetDlgItem(hDlg, IDC_CHECK1);
+
+}
+
+void InitButton(HWND hDlg, WPARAM wParam, LPARAM lParam)
+{
+    for (int i = 0; i < BUTTON_COUNT; i++)
+    {
+        Buttons.push_back(GetDlgItem(hDlg, IDC_BUTTON1 + i));
+        SetDlgItemText(hDlg, IDC_BUTTON1 + i, TEXT(" "));
+        EnableWindow(Buttons[i], TRUE);
+    }
+}
+
+
+void ResetGame(HWND hDlg, WPARAM wParam, LPARAM lParam) {
+    Check(hDlg, wParam, lParam);
+
+    chosen.clear();
+}
+
+
+void MakeComputerMove(HWND hDlg) {
+    srand(time(NULL));
+    int num;
+    if (chosen.size() != NULL)
+    {
+        do {
+            num = rand() % BUTTON_COUNT;
+        } while (find(chosen.begin(), chosen.end(), num) != chosen.end());
+    }
+    else
+    {
+        num = rand() % BUTTON_COUNT;
+    }
+    SetDlgItemText(hDlg, IDC_BUTTON1 + num, TEXT("O"));
+    EnableWindow(Buttons[num], FALSE);
+    chosen.push_back(num);
+
+    
+}
+
+void CheckForWinner(HWND hDlg, WPARAM wParam, LPARAM lParam) {
+    TCHAR buf[100];
+
+
+    //Х по вертикали
+    GetDlgItemText(hDlg, IDC_BUTTON1, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("X")) == 0) 
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON4, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("X")) == 0) 
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON7, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("X")) == 0) 
+            {
+                MessageBox(hDlg, TEXT("Вы победили!"), TEXT("Поздравляем!"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+    GetDlgItemText(hDlg, IDC_BUTTON2, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("X")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON5, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("X")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON8, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("X")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы победили!"), TEXT("Поздравляем!"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+    GetDlgItemText(hDlg, IDC_BUTTON3, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("X")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON6, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("X")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON9, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("X")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы победили!"), TEXT("Поздравляем!"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+    //О по вертикали
+
+    GetDlgItemText(hDlg, IDC_BUTTON1, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("O")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON4, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON7, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("O")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы проиграли!"), TEXT("Результат"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+    GetDlgItemText(hDlg, IDC_BUTTON2, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("O")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON5, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON8, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("O")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы проиграли!"), TEXT("Результат"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+    GetDlgItemText(hDlg, IDC_BUTTON3, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("O")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON6, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON9, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("O")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы проиграли!"), TEXT("Результат"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+
+    //Х по горизонтали
+    GetDlgItemText(hDlg, IDC_BUTTON4, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("X")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON5, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("X")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON6, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("X")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы победили!"), TEXT("Поздравляем!"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+
+    //О по горизонтали
+
+    GetDlgItemText(hDlg, IDC_BUTTON4, buf, sizeof(buf) / sizeof(buf[0]));
+    if (_tcscmp(buf, TEXT("O")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON4, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+
+            GetDlgItemText(hDlg, IDC_BUTTON4, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+        {
+            MessageBox(hDlg, TEXT("Вы проиграли :("), TEXT("Игра закончена!"), MB_OK);
+            ResetGame(hDlg, wParam, lParam);
+        }
+
+    }
+
+
+    //Х по диагонали
+    GetDlgItemText(hDlg, IDC_BUTTON1, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("X")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON5, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("X")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON9, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("X")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы победили!"), TEXT("Поздравляем!"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+    GetDlgItemText(hDlg, IDC_BUTTON3, buf, sizeof(buf) / sizeof(buf[0]));
+
+    if (_tcscmp(buf, TEXT("X")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON5, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("X")) == 0)
+        {
+            GetDlgItemText(hDlg, IDC_BUTTON7, buf, sizeof(buf) / sizeof(buf[0]));
+            if (_tcscmp(buf, TEXT("X")) == 0)
+            {
+                MessageBox(hDlg, TEXT("Вы победили!"), TEXT("Поздравляем!"), MB_OK);
+                ResetGame(hDlg, wParam, lParam);
+            }
+        }
+    }
+
+
+
+    //О по диагонали
+
+    GetDlgItemText(hDlg, IDC_BUTTON1, buf, sizeof(buf) / sizeof(buf[0]));
+    if (_tcscmp(buf, TEXT("O")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON5, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+
+            GetDlgItemText(hDlg, IDC_BUTTON9, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+        {
+            MessageBox(hDlg, TEXT("Вы проиграли :("), TEXT("Игра закончена!"), MB_OK);
+            ResetGame(hDlg, wParam, lParam);
+        }
+
+    }
+
+    GetDlgItemText(hDlg, IDC_BUTTON3, buf, sizeof(buf) / sizeof(buf[0]));
+    if (_tcscmp(buf, TEXT("O")) == 0)
+    {
+        GetDlgItemText(hDlg, IDC_BUTTON5, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+
+            GetDlgItemText(hDlg, IDC_BUTTON7, buf, sizeof(buf) / sizeof(buf[0]));
+        if (_tcscmp(buf, TEXT("O")) == 0)
+        {
+            MessageBox(hDlg, TEXT("Вы проиграли :("), TEXT("Игра закончена!"), MB_OK);
+            ResetGame(hDlg, wParam, lParam);
+        }
+
+    }
+
+}
+
+
+LRESULT CALLBACK DlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    int state = IsDlgButtonChecked(hDlg, IDC_CHECK1);
+
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+        InitButton(hDlg, wParam, lParam);
+        break;
+
+    case WM_COMMAND:
+        {
+            if (LOWORD(wParam) == IDCANCEL)
+            {
+                EndDialog(hDlg, 0);
+            } 
+
+            else if (LOWORD(wParam) == IDOK)
+            {
+                InitButton(hDlg, wParam, lParam);
+
+            }
+            else if (LOWORD(wParam) >= IDC_BUTTON1 && LOWORD(wParam) <= IDC_BUTTON1 + BUTTON_COUNT - 1) {
+                if (HIWORD(wParam) == BN_CLICKED) 
+                {
+
+                    if (state == BST_CHECKED)
+                    {
+                        for (int i = 0; i < Buttons.size(); i++) {
+                            if (LOWORD(wParam) == GetDlgCtrlID(Buttons[i])) {
+                                SetDlgItemText(hDlg, IDC_BUTTON1 + i, TEXT("X"));
+                                EnableWindow(Buttons[i], FALSE);
+                                chosen.push_back(i);
+                            }
+                        }
+
+                        CheckForWinner(hDlg, wParam, lParam);
+                        MakeComputerMove(hDlg);
+                        CheckForWinner(hDlg, wParam, lParam);
+                    }
+                    else if (state == BST_UNCHECKED)
+                    {
+                        MakeComputerMove(hDlg);
+                        CheckForWinner(hDlg, wParam, lParam);
+
+                        for (int i = 0; i < Buttons.size(); i++) {
+                            if (LOWORD(wParam) == GetDlgCtrlID(Buttons[i])) {
+                                SetDlgItemText(hDlg, IDC_BUTTON1 + i, TEXT("X"));
+                                EnableWindow(Buttons[i], FALSE);
+                                chosen.push_back(i);
+                            }
+                        }
+                        CheckForWinner(hDlg, wParam, lParam);
+                    }
+
+
+                    
+                }
+            }
+
+
+
+        }
+        break;
+    
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+        
+    }
+    return 0;
+}
+
+int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPTSTR lpszCmdLine, int nCmdShow)
+{
+    return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
+}
